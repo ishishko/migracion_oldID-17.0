643afdee4f111fc1cccab40f93347afba04cd926
Ignacio Shishko <shishkoignacio@gmail.com>
2024-04-22 11:40:37 -0300

    [FIX]Se realizan correcion de vista y manifest

--- migracion/__manifest__.py
+++ migracion/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    'name': 'Nombre del Módulo',
-    'description': 'Modulo para guardar Id de campo de BBDD de origen para poder realizar migraciones de datos historicos.',
+    'name': 'Campo Old_id Migracion',
+    'description': 'Modulo para guardar Id de campo de BBDD de origen para Optimizar migraciones de datos historicos. Valor encontrado en pestaña "Informacion Adicional"',
     'author': 'Ignacio Shishko',
     'version': '1.0.0',
     'category': 'Categoría del Módulo',
